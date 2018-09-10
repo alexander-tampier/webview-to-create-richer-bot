57195165eb9a9ebc432ca3576155b4183eadc511
Alexander Tampier <alexander.tampier@icloud.com>
2018-09-10 15:51:52 +0200

    fixed static public/ path

--- routes/options.js
+++ routes/options.js
@@ -4,16 +4,17 @@ const router = express.Router();
 
 // GET home page for the application
 router.get('/', (req, res) => {
-  console.log(JSON.stringify(req.query));
-
   const referer = req.get('Referer');
+
+  console.log(`Req.query: ${JSON.stringify(req.query)} | Referer: ${referer}, | Dirname: ${__dirname}`);
+
   if (referer) {
     if (referer.indexOf('www.messenger.com') >= 0) {
       res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
     } else if (referer.indexOf('www.facebook.com') >= 0) {
       res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
     }
-    res.sendFile('public/options.html', { root: __dirname });
+    res.sendFile('../public/options.html', { root: __dirname });
   }
 });
 
